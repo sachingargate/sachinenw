1bb06237391f23a640fbde09fd6479abb88d3c5e
sachin gargate <sachingargate@gmail.com>
2020-07-23 13:51:41 +0530

    one line change

---
+++ sachinfirst.php
@@ -0,0 +1,11 @@
+<?php
+
+$cURLConnection = curl_init();
+
+curl_setopt($cURLConnection, CURLOPT_URL, 'https://hostname.tld/phone-list');
+curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+$phoneList = curl_exec($cURLConnection);
+curl_close($cURLConnection);
+
+$jsonArrayResponse - json_decode($phoneList);
